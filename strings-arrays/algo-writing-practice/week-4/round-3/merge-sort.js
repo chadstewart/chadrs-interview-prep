454ee6402886c035fa4598790a05bdc5ec7f32bc
Chad Stewart <chadrhonanstewart@gmail.com>
2021-05-28 09:38:56 -0500

    Went through another round of spaced reptition remembering binary search, quick sort and marge sort. Outside of minor bugs, I remembered the algorithms pretty well. The next spaced reptition session for these algorithms would be next week, maybe Friday I think.

---
+++ strings-arrays/algo-writing-practice/week-4/round-3/merge-sort.js
@@ -0,0 +1,30 @@
+function mergeSort(array) {
+    if(!array) return null;
+
+    let { length } = array;
+
+    if(length > 1) {
+        let middle = Math.floor(length / 2);
+        let left = mergeSort(array.slice(0, middle));
+        let right = mergeSort(array.slice(middle));
+        array = merge(left, right);
+    }
+
+    return array;
+}
+
+function merge(left, right) {
+    let result = [];
+
+    while(left.length > 0 && right.length > 0) {
+        if(left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    return result.concat(left, right);
+}
+
+console.log(mergeSort([4,2,9,6]));
